7afb02e2103ee76edf1863a54bb4843f9c47ba31
skhan61 <sayem.eee.kuet@outlook.com>
2023-11-20 00:45:28 -0500

    Automatic commit at Mon 20 Nov 2023 12:45:28 AM EST

---
+++ auto_commit.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Navigate to your project repository
+cd "/home/sayem/Desktop/Hangman"
+
+# Explicitly add the 'Old' folder recursively
+find notebooks -type f \( -name "*.py" -o -name "*.ipynb" \) -exec git add -f {} \;
+find scr -type f \( -name "*.py" -o -name "*.ipynb" \) -exec git add -f {} \;
+# find report -type f -name "*.html" -exec git add -f {} \;
+
+# Check for changes in both working directory and staged area
+if [ -n "$(git diff)" ] || [ -n "$(git diff --cached)" ]; then
+    # Add all other changes not mentioned in .gitignore
+    git add .
+
+    # Commit the changes
+    git commit -m "Automatic commit at $(date)"
+
+    # Determine which branch to push based on the current active branch
+    CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+    if [ "$CURRENT_BRANCH" = "master" ]; then
+        git push git@github.com:skhan61/Optiver.git master
+    else
+        echo "You are currently on the $CURRENT_BRANCH branch. Please switch to the main branch to auto-push."
+    fi
+else
+    echo "No changes detected."
+fi
--- scr/game.py
+++ scr/game.py
@@ -1,30 +1,33 @@
+import random
+import re
+import string
+from collections import Counter
+
 import torch
 import torch.nn as nn
-import random
-from collections import Counter
-from scr.feature_engineering import * # process_single_word, get_missed_characters, char_to_idx, idx_to_char
 
-from scr.feature_engineering import process_single_word_inference, \
-    char_to_idx, idx_to_char
+# process_single_word, get_missed_characters, char_to_idx, idx_to_char
+from scr.feature_engineering import *
+from scr.feature_engineering import (
+    char_to_idx,
+    idx_to_char,
+    process_single_word_inference,
+)
 
 # Device configuration
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-import re
-from collections import Counter
-import random
-import string
 
 def get_random_character():
     # Choose from lowercase letters
-    characters = 'a' # string.ascii_lowercase  # 'abcdefghijklmnopqrstuvwxyz'
+    characters = 'a'  # string.ascii_lowercase  # 'abcdefghijklmnopqrstuvwxyz'
     random_char = random.choice(characters)
     return random_char
 
 
 # # def gues(self, word):
-def guess_character(model, masked_word, char_frequency, \
-    max_word_length, device, guessed_chars, fallback_strategy=True):
+def guess_character(model, masked_word, char_frequency,
+                    max_word_length, device, guessed_chars, fallback_strategy=True):
     """
     Guess the next character in the hangman game.
 
@@ -39,30 +42,33 @@ def guess_character(model, masked_word, char_frequency, \
     """
 
     # Preprocess the masked word
-    feature_set, missed_chars = process_single_word_inference(masked_word, char_frequency, max_word_length, normalize=True)
-    feature_set, missed_chars = feature_set.unsqueeze(0).to(device), missed_chars.unsqueeze(0).to(device)
-    sequence_lengths = torch.tensor([feature_set.size(1)], dtype=torch.long) # .to(device)
+    feature_set, missed_chars = process_single_word_inference(
+        masked_word, char_frequency, max_word_length, normalize=True)
+    feature_set, missed_chars = feature_set.unsqueeze(
+        0).to(device), missed_chars.unsqueeze(0).to(device)
+    sequence_lengths = torch.tensor(
+        [feature_set.size(1)], dtype=torch.long)  # .to(device)
 
     # Get model output
     with torch.no_grad():
         output = model(feature_set, sequence_lengths, missed_chars)
-    
+
     # Get probabilities of the last character position
     last_char_position = sequence_lengths.item() - 1
     probabilities = torch.softmax(output[0, last_char_position, :], dim=-1)
 
     # Exclude already guessed characters and get the most probable character
-    probabilities[torch.tensor([char_to_idx[char] for char in guessed_chars], \
-        dtype=torch.long)] = 0
+    probabilities[torch.tensor([char_to_idx[char] for char in guessed_chars],
+                               dtype=torch.long)] = 0
     best_char_index = torch.argmax(probabilities).item()
     guessed_char = idx_to_char[best_char_index]
     # guessed_char = get_random_character() #TODO
 
     # Fallback strategy: choose the most common unguessed character
     if fallback_strategy and guessed_char in \
-        guessed_chars or guessed_char == '_':
-        sorted_chars = sorted(char_frequency.items(), \
-            key=lambda x: x[1], reverse=True)
+            guessed_chars or guessed_char == '_':
+        sorted_chars = sorted(char_frequency.items(),
+                              key=lambda x: x[1], reverse=True)
         for char, _ in sorted_chars:
             if char not in guessed_chars:
                 guessed_char = char
@@ -76,14 +82,14 @@ def guess_character(model, masked_word, char_frequency, \
 # print(guessed_char)
 
 # # def gues(self, word):
-def guess(model, word, char_frequency, \
-    max_word_length, device, guessed_letters):
-    
-    cleaned_word = "".join(char.lower() \
-        for char in word if char.isalpha() or char == '_')
+def guess(model, word, char_frequency,
+          max_word_length, device, guessed_letters):
+
+    cleaned_word = "".join(char.lower()
+                           for char in word if char.isalpha() or char == '_')
 
     # Predict the next character using the updated guess_character function
-    guessed_char =  guess_character(
+    guessed_char = guess_character(
         model, cleaned_word,
         char_frequency,
         max_word_length,
@@ -96,16 +102,19 @@ def guess(model, word, char_frequency, \
     # Add the new guess to the guessed letters list
     if guessed_char not in guessed_letters:
         guessed_letters.append(guessed_char)
-    
+
     return guessed_char
 
 # mimic api
-def update_word_state(actual_word, current_state, guessed_char):
-    return ''.join([guessed_char if actual_word[i] == guessed_char else \
-        current_state[i] for i in range(len(actual_word))])
 
-def play_game_with_a_word(model, word, char_frequency, \
-    max_word_length, device, max_attempts=6, normalize=True):
+
+def update_word_state(actual_word, current_state, guessed_char):
+    return ''.join([guessed_char if actual_word[i] == guessed_char else
+                    current_state[i] for i in range(len(actual_word))])
+
+
+def play_game_with_a_word(model, word, char_frequency,
+                          max_word_length, device, max_attempts=6, normalize=True):
     guessed_letters = []  # A list to keep track of guessed characters
     attempts_remaining = max_attempts
     masked_word = "_" * len(word)
@@ -114,8 +123,10 @@ def play_game_with_a_word(model, word, char_frequency, \
     # print(f"Starting the game. Word to guess: {' '.join(masked_word)}")  # Display initial state
 
     while game_status == "ongoing" and attempts_remaining > 0:
-        guessed_char = guess(model, word, char_frequency, max_word_length, device, guessed_letters)
-        guessed_letters.append(guessed_char)  # Add to the list of guessed letters
+        guessed_char = guess(model, word, char_frequency,
+                             max_word_length, device, guessed_letters)
+        # Add to the list of guessed letters
+        guessed_letters.append(guessed_char)
         masked_word = update_word_state(word, masked_word, guessed_char)
 
         if guessed_char not in word:
@@ -136,7 +147,6 @@ def play_game_with_a_word(model, word, char_frequency, \
 
 #### Dont change above this ######
 
-import random
 
 def generate_masked_word_variants(word, max_variants, max_masks):
     word_length = len(word)
@@ -144,23 +154,27 @@ def generate_masked_word_variants(word, max_variants, max_masks):
     masked_versions = set()
 
     while len(masked_versions) < max_variants:
-        num_masks = random.randint(1, max_masks)  # Randomly choose the number of masks
+        # Randomly choose the number of masks
+        num_masks = random.randint(1, max_masks)
         mask_indices = set(random.sample(indices, num_masks))
-        masked_word = ''.join(c if i not in mask_indices \
-            else '_' for i, c in enumerate(word))
+        masked_word = ''.join(c if i not in mask_indices
+                              else '_' for i, c in enumerate(word))
         masked_versions.add(masked_word)
 
     return list(masked_versions)
+
 
 def process_word(word):
     word_length = len(word)
     max_variants = min(word_length, 10)  # Limit the number of variants
 
-    # Ensure at least one character is always revealed, except for the fully masked state
-    max_masks = max(1, int(word_length * 0.5))
+    # Ensure at least one character is always revealed,
+    # except for the fully masked state
+    max_masks = max(1, int(word_length * 0.8))
 
     # Generate other masked variants
-    other_masked_states = generate_masked_word_variants(word, max_variants - 1, max_masks)
+    other_masked_states = generate_masked_word_variants(
+        word, max_variants - 1, max_masks)
 
     # Ensure a fully masked state is always included
     initial_states = ['_' * word_length] + other_masked_states
@@ -168,79 +182,136 @@ def process_word(word):
     return initial_states
 
 
-import random
+def simulate_game_progress(model, word, initial_state,
+                           char_frequency, max_word_length, device, max_attempts=6,
+                           normalize=True, difficulty="medium", outcome_preference=None):
 
-def simulate_game_progress(model, word, \
-    initial_state, char_frequency, max_word_length, \
-        device, max_attempts=6, normalize=True, difficulty="medium", \
-            outcome_preference=None):
-    
     guessed_letters = []
     attempts_remaining = max_attempts
     masked_word = initial_state  # Starting with the initial masked state of the word
 
-    # Set difficulty level
+    # Set difficulty level and print it
     difficulty_levels = {"easy": 0.8, "medium": 0.5, "hard": 0.2}
     correct_guess_chance = difficulty_levels.get(difficulty, 0.5)
+
+    # print(
+    #     f"Starting game with difficulty: {difficulty}, \
+    #         Correct guess chance: {correct_guess_chance}")
 
     game_progress = []
 
     while attempts_remaining > 0:
-        possible_correct_guesses = [char for char in word if char not in guessed_letters and char.isalpha()]
-        possible_incorrect_guesses = [char for char in 'abcdefghijklmnopqrstuvwxyz' if char not in word and char not in guessed_letters]
+        # print(
+        #     f"Current state: {masked_word}, Attempts remaining: \
+        #         {attempts_remaining}, Guessed letters: {guessed_letters}")
 
-        # Check if no more characters left to guess
-        if not possible_correct_guesses and not possible_incorrect_guesses:
-            print("No more characters left to guess.")
-            break
+        possible_correct_guesses = [
+            char for char in word if char not in guessed_letters and char.isalpha()]
+        possible_incorrect_guesses = [
+            char for char in 'abcdefghijklmnopqrstuvwxyz'
+            if char not in word and char not in guessed_letters]
 
+        # # Print possible guesses
+        # print(f"Possible correct guesses: {possible_correct_guesses}")
+        # print(f"Possible incorrect guesses: {possible_incorrect_guesses}")
+
+        # Decision making based on outcome preference
         if outcome_preference == "win" and possible_correct_guesses:
-            guessed_char = random.choice(possible_correct_guesses) \
-                if random.random() < correct_guess_chance else random.choice(possible_incorrect_guesses)
+            guessed_char = random.choice(possible_correct_guesses) if random.random(
+            ) < correct_guess_chance else random.choice(possible_incorrect_guesses)
         elif outcome_preference == "lose" and possible_incorrect_guesses:
-            guessed_char = random.choice(possible_incorrect_guesses) \
-                if random.random() > correct_guess_chance else random.choice(possible_correct_guesses)
+            guessed_char = random.choice(possible_incorrect_guesses) if random.random(
+            ) > correct_guess_chance else random.choice(possible_correct_guesses)
         else:
-            guessed_char = random.choice(possible_correct_guesses \
-                + possible_incorrect_guesses) if \
-                    (possible_correct_guesses + possible_incorrect_guesses) else None
+            guessed_char = random.choice(possible_correct_guesses + possible_incorrect_guesses) if (
+                possible_correct_guesses + possible_incorrect_guesses) else None
 
         if not guessed_char:
+            # print("No valid character to guess, skipping turn.")
             continue
+
+        # Print the guessed character
+        # print(f"Guessed character: {guessed_char}")
 
         guessed_letters.append(guessed_char)
         new_masked_word = update_word_state(word, masked_word, guessed_char)
         correct_guess = guessed_char in word
 
+        # Print the result of the guess
+        # print(
+        #     f"Result of guess: {'Correct' if correct_guess else 'Incorrect'}, \
+        #         New masked word: {new_masked_word}")
+
         game_progress.append((guessed_char, new_masked_word, correct_guess))
 
         if new_masked_word == word:
+            # print("Word guessed correctly.")
             return True, game_progress
         elif not correct_guess:
             attempts_remaining -= 1
 
         masked_word = new_masked_word
 
+    # print("Game ended. Word not guessed correctly.")
     return masked_word == word, game_progress
 
 
+# import random
 
+# def simulate_game_progress(model, word, \
+#     initial_state, char_frequency, max_word_length, \
+#         device, max_attempts=6, normalize=True, difficulty="medium", \
+#             outcome_preference=None):
 
+#     guessed_letters = []
+#     attempts_remaining = max_attempts
+#     masked_word = initial_state  # Starting with the initial masked state of the word
 
+#     # Set difficulty level
+#     difficulty_levels = {"easy": 0.8, "medium": 0.5, "hard": 0.2}
+#     correct_guess_chance = difficulty_levels.get(difficulty, 0.5)
 
+#     game_progress = []
 
+#     while attempts_remaining > 0:
+#         possible_correct_guesses = [char for char in word if \
+#             char not in guessed_letters and char.isalpha()]
+#         possible_incorrect_guesses = [char for char in \
+#             'abcdefghijklmnopqrstuvwxyz' if char not in word and char not in guessed_letters]
 
+#         # Check if no more characters left to guess
+#         if not possible_correct_guesses and not possible_incorrect_guesses:
+#             print("No more characters left to guess.")
+#             break
 
+#         if outcome_preference == "win" and possible_correct_guesses:
+#             guessed_char = random.choice(possible_correct_guesses) \
+#                 if random.random() < correct_guess_chance else random.choice(possible_incorrect_guesses)
+#         elif outcome_preference == "lose" and possible_incorrect_guesses:
+#             guessed_char = random.choice(possible_incorrect_guesses) \
+#                 if random.random() > correct_guess_chance else random.choice(possible_correct_guesses)
+#         else:
+#             guessed_char = random.choice(possible_correct_guesses \
+#                 + possible_incorrect_guesses) if \
+#                     (possible_correct_guesses + possible_incorrect_guesses) else None
 
+#         if not guessed_char:
+#             continue
 
+#         guessed_letters.append(guessed_char)
+#         new_masked_word = update_word_state(word, masked_word, guessed_char)
+#         correct_guess = guessed_char in word
 
+#         game_progress.append((guessed_char, new_masked_word, correct_guess))
 
+#         if new_masked_word == word:
+#             return True, game_progress
+#         elif not correct_guess:
+#             attempts_remaining -= 1
 
+#         masked_word = new_masked_word
 
-
-
-
-
+#     return masked_word == word, game_progress
 
 
 # def simulate_game_progress(model, word, initial_state, \
@@ -296,10 +367,6 @@ def simulate_game_progress(model, word, \
 #         masked_word = new_masked_word  # Update for next iteration
 
 #     return masked_word == word, game_progress
-
-
-
-
 
 
 # def simulate_game_progress(model, word, char_frequency, \
@@ -358,5 +425,3 @@ def simulate_game_progress(model, word, \
 #         masked_word = new_masked_word
 
 #     return False, game_progress  # Return the entire game progress
-
-
